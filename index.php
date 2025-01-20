0cec3fd25073520ddcec3b43e0286e3a5d937eb3
Josh011600 <saculojosh1600@gmail.com>
2025-01-20 14:40:12 +0800

    commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN PAGE</title>
+    <link rel="stylesheet" href="css/styles.css">
+    
+</head>
+<body>
+<!--COMMENT -->
+<!--START HERE! -->
+    <form class="login" action="login.php" method="post"> 
+        
+    <h1 class="welcome">Login Form</h1><br>
+    <label for="username">username</label><br>
+    <!-- name=username is for login.php if(isset username etc.)-->
+    <input type="text" name="username" placeholder="username"required></input><br>
+    <label for="password" >password</label><br>
+     <!-- name=password is for login.php if(isset password etc.)-->
+    <input type="password " name="password" placeholder="password" required></input><br>
+    <button type="submit">Login</button><br>
+    <label for="signup">not registered yet?</label><br>
+    <button><a href="register.php">signup</a></button>
+</form>
+
+
+</body>
+<?php 
+    include 'loginfunction/Connection.php';
+    ?>
+</html>
+
---
+++ login.php
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<form action = "" method = "post">
+               <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+               <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+               <input type = "submit" value = " Submit "/><br />
+            </form>
+        <div style = "font-size:11px; color:#cc0000; margin-top:10px">
+			<?php  ?>
+		</div>
+</body>
+</html>
+<script>
+	
+
+
+</script>
