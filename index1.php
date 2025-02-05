be7f56b4b8edef887dc16dc119f8262d37fb5616
Josh011600 <saculojosh1600@gmail.com>
2025-02-05 10:52:28 +0800

    commit

---
+++ index1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hello</title>
+</head>
+<body>
+<form>
+<label>Num1</label>
+<input type="text" name="num1" placeholder="Enter a number">
+<label>Num2</label>
+<input type="text" name="num2" placeholder="Enter 2nd number">
+<button>ADD</button>
+<label>Result:</label><input type="text" name="result"></form>
+
+</body>
+</html>
+<?php
+echo "<label>Num1</label>";
+echo "<input type='text' name='num1' placeholder='Enter a number'>";
+
+?>
